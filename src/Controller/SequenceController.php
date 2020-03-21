fccae0dc4b435aa9989e4b860438f9c3b85f6065
zygmunt.loter <zygmunt.loter@vao.pl>
2020-03-21 22:00:06 +0100

    Base structure for sequence Controller & Service

---
+++ src/Controller/SequenceController.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use App\Services\SequenceService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class SequenceController extends AbstractController
+{
+    /**
+     * @var SequenceService
+     */
+    private $sequenceService;
+
+    /**
+     * SequenceController constructor.
+     */
+    public function __construct()
+    {
+        $this->sequenceService = new SequenceService();
+        parent::__construct();
+    }
+
+    /**
+     * @return Response
+     */
+    public function formAction() {
+        return new Response('Response');
+    }
+}
---
+++ src/Services/SequenceService.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace App\Services;
+
+
+class SequenceService
+{
+    /**
+     * @param int $number Length of sequence
+     * @return array Full sequence
+     */
+    private function createSequence(int $number) : array {
+        return [];
+    }
+
+    /**
+     * @param int $number Length of sequence
+     * @return int Max value in sequence
+     */
+    public function findSequenceMax(int $number) : int {
+        return 0;
+    }
+
+}
